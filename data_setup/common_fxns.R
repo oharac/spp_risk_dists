806f5ab8a5eb6646461076848426149ef038c24b
Casey O'Hara <ohara@nceas.ucsb.edu>
2018-07-21 08:44:18 -0700

    updating a bit based on Ben's comments and chat with Gina.  Also: rerunning with 2018-1 values
    Former-commit-id: baf2914ec90f3337084584d6b9f380766f9b975b
    Former-commit-id: b6f3a7bf8f67ffcdc65dd734c451aa70f0ae491d

--- data_setup/common_fxns.R
+++ data_setup/common_fxns.R
@@ -47,6 +47,48 @@ clip_to_globe <- function(x) {
   return(z)
 }
 
+valid_check <- function(spp_shp, spp) {
+  valid <- st_is_valid(spp_shp)
+  ### can return a vector if multiple polygons with same ID
+  if(any(!valid)) {
+    cat_msg('Found invalid geometries in ', spp)
+    
+    bbox_shp <- st_bbox(spp_shp)
+    if(bbox_shp$xmin < -180 | bbox_shp$xmax > 180) {
+      cat_msg('Bounding box outside +/- 180; buffering with dist = 0')
+      ### check area before and after buffering to make sure no loss
+      area_pre  <- st_area(spp_shp) %>% as.numeric() / 1e6
+      spp_shp   <- st_buffer(spp_shp, dist = 0) %>%
+        st_cast('MULTIPOLYGON')
+      area_post <- st_area(spp_shp) %>% as.numeric() / 1e6
+      
+      area_check <- all.equal(area_pre, area_post)
+      area_ratio <- max(sum(area_pre) / sum(area_post),
+                        sum(area_post) / sum(area_pre))
+      
+      
+      ### error check to make sure the buffer didn't lose polygons
+      if(area_check == FALSE | 
+         (class(area_check) != 'logical' & area_ratio > 1.001)) {
+        ### use all.equal() for near equality, and for comparing all 
+        ### elements in case of a vector.  If a difference, choose an arbitrary
+        ### threshold for "close enough".
+        cat_msg('Error: iucn_sid: ', spp, ': area_pre = ', round(sum(area_pre), 3), 
+                '; area_post = ', round(sum(area_post), 3), 
+                '; area_ratio = ', round(area_ratio, 5), '; not equal!')
+        stop('Area_pre and area_post not equal!')
+      } else {
+        cat_msg('Area check good! iucn_sid: ', spp, ': area_pre = ', round(sum(area_pre), 3), 
+                '; area_post = ', round(sum(area_post), 3), 
+                '; area_ratio = ', round(area_ratio, 5), '; all equal!')
+      }
+    } else {
+      cat_msg('bbox not exceeded; no need to fix polygon with buffer')
+    }
+  }
+  return(spp_shp)
+}
+
 
 ### Functions for accessing IUCN API
 
