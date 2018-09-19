2975a5ef0fb4108b482978cf35063c72929b0adc
Casey O'Hara <ohara@nceas.ucsb.edu>
2018-09-19 15:57:17 -0700

    Updating README.md.  For file org, added underscore to main analysis folders...
    also cleaning out a bunch of old stuff no longer used.
    
    
    Former-commit-id: 591744d2ff2f7dfcecf6247f3f69648395b8e3b4
    Former-commit-id: eb872ffd78d96405b956d9ae62aa4114d9905690

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# spp_health_dists
-Code to support exploration of distribution patterns of species extinction risk 
+# Mapping status and conservation of global at-risk marine biodiversity
+
+__Authors:__ Casey C. O'Hara^1,2^, Juan Carlos Villaseñor-Derbez^1^, Gina M. Ralph^3^, Benjamin S. Halpern^1,2,4^
+
+1. Bren School of Environmental Science and Management, University of California, Santa Barbara CA 93106
+2. National Center for Ecological Analysis and Synthesis, University of California, 735 State Street Suite 300, Santa Barbara CA 93101
+3. IUCN Marine Biodiversity Unit, Department of Biological Sciences, Old Dominion University, Norfolk, VA 23529, USA
+4. Department of Life Sciences, Imperial College London, Silwood Park Campus, Buckhurst Rd, Ascot, West Berkshire SL5 7PY, United Kingdom
+
+\* Correspondence to: Casey O'Hara; cohara@bren.ucsb.edu
+
+## Abstract: 
+
+To conserve marine biodiversity we must first understand the spatial distribution and status of at-risk biodiversity.
+We combined range maps and conservation status for `r fnum(n_spp)` marine species to map the global distribution of extinction risk of marine biodiversity.
+We find that for `r fnum(pct_threat_quarter, 0)`% of the ocean, >25% of assessed species are considered threatened, and `r fnum(pct_rr_threat_half, 0)`% of the ocean shows >50% of assesed species threatened when weighting for range-limited species.
+Comparing marine biodiversity risk to locations of no-take marine reserves, we identify regions where reserves preferentially afford proactive protection (i.e., preserving low-risk areas) or reactive protection (i.e., mitigating high-risk areas).
+In particular, elevated risk to high seas biodiversity indicates a need for credible protection and reduction of fishing effort in international waters.
+
+## This repository
+
+Welcome to the repository for code and data related to __Mapping status and conservation of global at-risk marine biodiversity__.  Here is an overview of the organization of files and data:
+
+### Code
+
+#### Run this first! Setup directory: `spp_risk_dists/_setup`
+
+* In this directory are a sequence of files used to generate the bits and pieces that are later assembled into the rasters of biodiversity risk.
+* .Rmd files are sequenced with a prefix number (and letter) to indicate the order of operations.  Briefly:
+    1. Pull information from the IUCN Red List API to determine an overall species list, habitat information, and current risk (conservation status).
+    2. Pull information from API on risk from regional assessments; also recode the regions according to Marine Ecoregions (Spalding et al, 2007) for later spatialization.
+    3. Pull historical assessment information from API for possible trend analysis.  Note that this did not make it into the final draft of the manuscript.
+    4. Set up spatial layers in Gall-Peters, 100 km^2^ cells.  Layers include:
+        * cell ID (cells are sequentially numbered for combining with tabular data)
+        * ocean area
+        * marine protected area (classification, year of protection, proportion of protection)
+        * Exclusive Economic Zones (EEZ) and FAO fishing regions
+        * Marine Ecoregions of the World
+        * bathymetry
+        * NOTE: these layers are all saved in the `spp_risk_dists/_spatial` directory.
+    5. Convert species range maps to rasters.
+        * For maps provided directly by IUCN, aggregate into multispecies files based on family.  There is some cleaning done at this stage to fix problematic extents and attributes.
+        * From the list of all available maps, generate a master list of all mapped, assessed species for inclusion in the study.
+        * Rasterize each species to a .csv that includes cell ID and presence.  A .csv format was used for file size and ease of reading and binding into dataframes.
+    6. Aggregate individual species ranges into larger taxonomic groups, and summarize key variables (mean risk, variance of risk, number of species, etc) by group.  
+        * Technically this is not necessary but makes it easier to quality check the process along the way, and supports mapping at the level of taxonomic group rather than the entire species list level.
+        * This process is done twice: once for uniform weighting and once for range-rarity weighting.  Resulting files are saved separately.
+
+#### Then run this!  Root directory: `spp_risk_dists`
+
+* At this level there are several scripts, prefixed `1x_biodiversity_maps`, that collate the various taxonomic group level files (generated in `setup` part 6) and summarize to the global level.  
+    * Note each creates a specific aggregation - comprehensively assessed species vs all available species; uniform vs range-rarity weighting.
+    * The rasters generated in these scripts are saved in the `_output` folder.
+* At this level are also all scripts used to generate figures for the manuscript, based on the data from the setup scripts and the rasters.
+    * The figures are saved in `spp_risk_dists/ms_figures` directory.
+  
+### Data and output files
+
+* The `spp_risk_dists/_data` folder contains tabular data about IUCN species used throughout the processing of this analysis.  These files are generated by scripts in the setup directory.
+* The `spp_risk_dists/_spatial` folder contains general spatial data generated and/or used in the `setup` scripts.  These include:
+    * rasters for cell ID, EEZ ID, marine ecoregion ID, ocean area, and bathymetry masks.   
+    * tabular data of region names and lookups for IUCN regional assessment to marine ecoregion.
+    * tabular data of marine protected area level/year/coverage to cell ID.
+    * shapefiles used for map plotting from Natural Earth.
+* The `spp_risk_dists/_output` folder contains the rasters of biodiversity risk, species richness, variance of risk, etc generated from the scripts in the base directory.
+
+
---
+++ _setup/common_fxns.R
@@ -0,0 +1,222 @@
+### Support functions for this project
+
+### * IUCN API functions
+### * Simple Features and Raster common functions
+### * Other random helper functions
+
+### common plot functions/variables
+### This color scheme and label scheme matches the mean risk maps
+risk_cols <- c('green4', viridis::inferno(6, direction = -1))
+risk_vals <- c(0, .2, .3, .4, .5, .7, 1.0)
+risk_lbls <- c('LC', 'NT', 'VU', 'EN', 'CR', 'EX')
+risk_brks <- c( 0.0,  0.2,  0.4,  0.6,  0.8,  1.0)
+
+### This color scheme and label scheme matches the pct threatened maps
+thr_cols <- viridis::viridis(6, direction = +1)
+thr_vals <- c(0, .25, .33, .42, .50, 1)
+### much of the density between .25 and .50
+thr_brks <- c(0, .25, .50, .75, 1)
+thr_lbls <- c('0%', '25%', '50%', '75%', '100%')
+
+
+### cat if not knitting; message if knitting
+
+cat_msg <- function(x, ...) {
+  if(is.null(knitr:::.knitEnv$input.dir)) {
+    ### not in knitr environment, so use cat()
+    cat(x, ..., '\n')
+  } else {
+    ### in knitr env, so use message()
+    message(x, ...)
+  }
+  return(invisible(NULL))
+}
+
+### Simple Features functions
+gp_proj4 <- '+proj=cea +lon_0=0 +lat_ts=45 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs'
+
+
+clip_to_globe <- function(x) {
+  ### for SF features, transform to wgs84, clip to +-180 and +-90
+  epsg <- st_crs(x)$epsg
+  if(epsg != 4326 | is.na(epsg)) {
+    message('Original EPSG = ', epsg, '; Proj4 = ', st_crs(x)$proj4string,
+            '\n...converting to EPSG:4326 WGS84 for clipping')
+    x <- st_transform(x, 4326)
+  }
+  x_bbox <- st_bbox(x)
+  if(x_bbox$xmin < -180 | x_bbox$xmax > +180 |
+     x_bbox$ymin <  -90 | x_bbox$ymax >  +90) {
+    message('Some bounds outside +-180 and +-90 - clipping')
+    z <- st_crop(x, y = c('xmin' = -180,
+                          'ymin' =  -90,
+                          'xmax' = +180,
+                          'ymax' =  +90)) %>%
+      st_cast('MULTIPOLYGON')
+    # z <- as(x, 'Spatial') %>%
+    #   raster::crop(raster::extent(-180, 180, -90, 90)) %>%
+    #   st_as_sf()
+    ### otherwise is sfc_GEOMETRY which doesn't play well with fasterize.
+    ### The st_crop solution works great most of the time; but for
+    ### spp 21132910 (at least) the crop turned things into linestrings
+    ### that couldn't be converted with st_cast('MULTIPOLYGON').
+  } else {
+    message('All bounds OK, no clipping necessary')
+    z <- x
+  }
+  return(z)
+}
+
+clip_to_globe_sp <- function(x) {
+  ### Convert to SP and use raster::crop - this is a backup when the 
+  ### sf method (st_crop) fails miserbably
+  epsg <- st_crs(x)$epsg
+  if(epsg != 4326 | is.na(epsg)) {
+    message('Original EPSG = ', epsg, '; Proj4 = ', st_crs(x)$proj4string,
+            '\n...converting to EPSG:4326 WGS84 for clipping')
+    x <- st_transform(x, 4326)
+  }
+  x_bbox <- st_bbox(x)
+  if(x_bbox$xmin < -180 | x_bbox$xmax > +180 |
+     x_bbox$ymin <  -90 | x_bbox$ymax >  +90) {
+    message('Some bounds outside +-180 and +-90 - clipping')
+    z <- as(x, 'Spatial') %>%
+      raster::crop(raster::extent(-180, 180, -90, 90)) 
+    z_sf <- z %>%
+      st_as_sf()
+    
+    return(z_sf)
+    
+  } else {
+    message('All bounds OK, no clipping necessary')
+    z <- x
+  }
+  return(z)
+}
+
+valid_check <- function(spp_shp) {
+  valid <- st_is_valid(spp_shp)
+  ### can return a vector if multiple polygons with same ID
+  if(any(!valid)) {
+    cat_msg('Found invalid geometries')
+    
+    bbox_shp <- st_bbox(spp_shp)
+    if(bbox_shp$xmin < -180 | bbox_shp$xmax > 180) {
+      cat_msg('Bounding box outside +/- 180; buffering with dist = 0')
+      ### check area before and after buffering to make sure no loss
+      area_pre  <- st_area(spp_shp) %>% as.numeric() / 1e6
+      spp_shp   <- st_buffer(spp_shp, dist = 0) %>%
+        st_cast('MULTIPOLYGON')
+      area_post <- st_area(spp_shp) %>% as.numeric() / 1e6
+      
+      area_check <- all.equal(area_pre, area_post)
+      area_ratio <- max(sum(area_pre) / sum(area_post),
+                        sum(area_post) / sum(area_pre))
+      
+      
+      ### error check to make sure the buffer didn't lose polygons
+      if(area_check == FALSE | 
+         (class(area_check) != 'logical' & area_ratio > 1.001)) {
+        ### use all.equal() for near equality, and for comparing all 
+        ### elements in case of a vector.  If a difference, choose an arbitrary
+        ### threshold for "close enough".
+        cat_msg('Error: area_pre = ', round(sum(area_pre), 3), 
+                '; area_post = ', round(sum(area_post), 3), 
+                '; area_ratio = ', round(area_ratio, 5), '; not equal!')
+        stop('Area_pre and area_post not equal!')
+      } else {
+        cat_msg('Area check good!  area_pre = ', round(sum(area_pre), 3), 
+                '; area_post = ', round(sum(area_post), 3), 
+                '; area_ratio = ', round(area_ratio, 5), '; all equal!')
+      }
+    } else {
+      cat_msg('bbox not exceeded; no need to fix polygon with buffer')
+    }
+  }
+  return(spp_shp)
+}
+
+
+### Functions for accessing IUCN API
+
+### `get_from_api()` is a function to access the IUCN API, given a url 
+### (specific to data sought), parameter, and key (stored in ohi/git-annex), 
+### as well as a delay if desired (to ease the load on the IUCN API server). 
+### `mc_get_from_api()` runs the `get_from_api()` function across multiple 
+### cores for speed...
+
+
+library(parallel)
+library(jsonlite)
+
+### api_key stored on git-annex so outside users can use their own key
+api_file <- file.path(dir_M, 'git-annex/globalprep/spp_ico', 
+                      'api_key.csv')
+api_key <- scan(api_file, what = 'character')
+
+api_version <- fromJSON('http://apiv3.iucnredlist.org/api/v3/version') %>%
+  .$version
+
+# api_version <- '2017-3'
+
+
+get_from_api <- function(url, param, api_key, delay) {
+  
+  i <- 1; tries <- 5; success <- FALSE
+  
+  while(i <= tries & success == FALSE) {
+    message('try #', i)
+    Sys.sleep(delay * i) ### be nice to the API server? later attempts wait longer
+    api_info <- fromJSON(sprintf(url, param, api_key)) 
+    if (class(api_info) != 'try-error') {
+      success <- TRUE
+    } else {
+      warning(sprintf('try #%s: class(api_info) = %s\n', i, class(api_info)))
+    }
+    message('... successful? ', success)
+    i <- i + 1
+  }
+  
+  if (class(api_info) == 'try-error') { ### multi tries and still try-error
+    api_return <- data.frame(param_id  = param,
+                             api_error = 'try-error after multiple attempts')
+  } else if (class(api_info$result) != 'data.frame') { ### result isn't data frame for some reason
+    api_return <- data.frame(param_id  = param,
+                             api_error = paste('non data.frame output: ', class(api_info$result), ' length = ', length(api_info$result)))
+  } else if (length(api_info$result) == 0) { ### result is empty
+    api_return <- data.frame(param_id  = param,
+                             api_error = 'zero length data.frame')
+  } else {
+    api_return <- api_info %>%
+      data.frame(stringsAsFactors = FALSE)
+  }
+  
+  return(api_return)
+}
+
+mc_get_from_api <- function(url, param_vec, api_key, cores = NULL, delay = 0.5) {
+  
+  if(is.null(cores)) 
+    numcores <- ifelse(Sys.info()[['nodename']] == 'mazu', 12, 1)
+  else 
+    numcores <- cores
+  
+  out_list <- parallel::mclapply(param_vec, 
+                                 function(x) get_from_api(url, x, api_key, delay),
+                                 mc.cores   = numcores,
+                                 mc.cleanup = TRUE) 
+  
+  if(any(sapply(out_list, class) != 'data.frame')) {
+    error_list <- out_list[sapply(out_list, class) != 'data.frame']
+    message('List items are not data frame: ', paste(sapply(error_list, class), collapse = '; '))
+    message('might be causing the bind_rows() error; returning the raw list instead')
+    return(out_list)
+  }
+  
+  out_df <- out_list %>%
+    bind_rows()
+  out_df <- out_df %>%
+    setNames(names(.) %>%
+               str_replace('result.', ''))
+  return(out_df)
+}
